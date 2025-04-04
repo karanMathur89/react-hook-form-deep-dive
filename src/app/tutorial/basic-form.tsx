1f2795f17cf8c29d8a1f1a740ced13873ff7014d
karanMathur89 <74524108+karanMathur89@users.noreply.github.com>
2025-04-04 11:03:04 +0530

    feat: implement basic form with username, email, and channel fields using react-hook-form

--- src/app/tutorial/basic-form.tsx
+++ src/app/tutorial/basic-form.tsx
@@ -1,7 +1,42 @@
 "use client"
 
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+
 export default function BasicForm() {
+  //* USEFORM
+  const form = useForm()
+  const { register } = form
+
   return (
-    <form className="space-y-4 bg-gray-100 shadow px-4 py-12 rounded-md"></form>
+    <div>
+      <form className="space-y-4 bg-gray-100 shadow px-4 py-12 rounded-md">
+        <div>
+          <Label>
+            {" "}
+            Username
+            <Input {...register("username")} />
+          </Label>
+        </div>
+        <div>
+          <Label>
+            {" "}
+            Email
+            <Input type="email" {...register("email")} />
+          </Label>
+        </div>
+        <div>
+          <Label>
+            {" "}
+            Channel
+            <Input {...register("channel")} />
+          </Label>
+        </div>
+        <Button>Submit</Button>
+      </form>
+    </div>
   )
 }
